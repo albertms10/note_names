f0dd25122d567a3e4298b1d3725fbd9358fefac5
Albert Mañosa <albertmasa2@gmail.com>
2023-06-03 01:39:00 +0200

    test(main): add `main` test call

--- test/model/main.dart
+++ test/model/main.dart
@@ -1,7 +1,10 @@
 import 'alphabet_test.dart' as alphabet_test;
+import 'cryptogram_string_extension_test.dart'
+    as cryptogram_string_extension_test;
 import 'name_test.dart' as name_test;
 
 void main() {
   alphabet_test.main();
+  cryptogram_string_extension_test.main();
   name_test.main();
 }
