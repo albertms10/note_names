6ed2abe90fc129477e96d5faa503399a173ed52e
Albert Mañosa <albertmasa2@gmail.com>
2023-06-03 01:59:37 +0200

    feat(model): add `CryptogramScheme` class and `toCryptogram` extension method (#9)
    * feat(model): add `CryptogramScheme` and `toCryptogram` extension method
    
    * chore(pubspec): remove `characters` dependency
    
    * test(cryptogram_string_extension): add test cases for extension method
    
    * chore(gitignore): ignore `bin` directory
    
    * test(main): add `main` test call
    
    * test(cryptogram_string_extension): add missing `()` to group name
    
    * test(cryptogram_string_extension): add test case for solmization
    
    * docs(cryptogram_string_extension): fix reference caps
    
    * refactor(cryptogram_string_extension): remove unused const constructor
    
    * test(cryptogram_string_extension): add more test cases

---
+++ test/model/cryptogram_string_extension_test.dart
@@ -0,0 +1,86 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:music_notes/music_notes.dart';
+import 'package:note_names/model/cryptogram_scheme.dart';
+
+void main() {
+  group('CryptogramStringExtension', () {
+    group('.toCryptogram()', () {
+      test('should return the musical cryptogram of this String', () {
+        final herculesDuxFerrarie = 'Hercules Dux Ferrarie'
+            .toCryptogram(CryptogramScheme.solmization());
+        expect(
+          herculesDuxFerrarie,
+          [Note.d, Note.c, Note.d, Note.c, Note.d, Note.f, Note.e, Note.d],
+        );
+
+        final arnoldSchonberg =
+            'A. SCHBEG'.toCryptogram(CryptogramScheme.german());
+        expect(
+          arnoldSchonberg,
+          [Note.a, Note.e.flat, Note.c, Note.b, Note.b.flat, Note.e, Note.g],
+        );
+
+        final belaBartok = 'BE BA'.toCryptogram(CryptogramScheme.french());
+        expect(belaBartok, [Note.b, Note.e, Note.b, Note.a]);
+
+        final carlPhilipEmmanuelBach =
+            'CFE BACH'.toCryptogram(CryptogramScheme.german());
+        expect(
+          carlPhilipEmmanuelBach,
+          [Note.c, Note.f, Note.e, Note.b.flat, Note.a, Note.c, Note.b],
+        );
+
+        final dimitriSchostakowitsch =
+            'D. SCH'.toCryptogram(CryptogramScheme.german());
+        expect(dimitriSchostakowitsch, [Note.d, Note.e.flat, Note.c, Note.b]);
+
+        final edvardGried = 'EBG'.toCryptogram(CryptogramScheme.french());
+        expect(edvardGried, [Note.e, Note.b, Note.g]);
+
+        final franzSchubert = 'F. SCH'.toCryptogram(CryptogramScheme.german());
+        expect(franzSchubert, [Note.f, Note.e.flat, Note.c, Note.b]);
+
+        final gustavHolst = 'GSAH'.toCryptogram(CryptogramScheme.german());
+        expect(gustavHolst, [Note.g, Note.e.flat, Note.a, Note.b]);
+
+        final jehanAlain =
+            'ALAIN'.toCryptogram(CryptogramScheme.frenchVariant());
+        expect(jehanAlain, [Note.a, Note.d, Note.a, Note.a, Note.f]);
+
+        final johannesBrahms = 'BAHS'.toCryptogram(CryptogramScheme.german());
+        expect(johannesBrahms, [Note.b.flat, Note.a, Note.b, Note.e.flat]);
+
+        final johannSebastianBach =
+            'BACH'.toCryptogram(CryptogramScheme.german());
+        expect(johannSebastianBach, [Note.b.flat, Note.a, Note.c, Note.b]);
+
+        final johnCage = 'CAGE'.toCryptogram(CryptogramScheme.german());
+        expect(johnCage, [Note.c, Note.a, Note.g, Note.e]);
+
+        final josephHaydn = 'HAYDN'.toCryptogram(CryptogramScheme.german());
+        expect(josephHaydn, [Note.b, Note.a, Note.d, Note.d, Note.g]);
+
+        final metaAbegg = 'ABEGG'.toCryptogram(CryptogramScheme.german());
+        expect(metaAbegg, [Note.a, Note.b.flat, Note.e, Note.g, Note.g]);
+
+        final nielsGade = 'GADE'.toCryptogram(CryptogramScheme.german());
+        expect(nielsGade, [Note.g, Note.a, Note.d, Note.e]);
+
+        final paulSacher = 'SACHER'.toCryptogram(CryptogramScheme.german());
+        expect(
+          paulSacher,
+          [Note.e.flat, Note.a, Note.c, Note.b, Note.e, Note.d],
+        );
+
+        final robertSchumann = 'SCHA'.toCryptogram(CryptogramScheme.german());
+        expect(robertSchumann, [Note.e.flat, Note.c, Note.b, Note.a]);
+
+        final asch1 = 'ASCH'.toCryptogram(CryptogramScheme.german());
+        expect(asch1, [Note.a.flat, Note.c, Note.b]);
+
+        final asch2 = 'A SCH'.toCryptogram(CryptogramScheme.german());
+        expect(asch2, [Note.a, Note.e.flat, Note.c, Note.b]);
+      });
+    });
+  });
+}
